bf682af07540f29edaed43316f1b78940c5e6e5f
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-09-22 02:16:45 +0200

    auth middleware, get userData  done

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 const bcryptjs = require('bcryptjs');
 const authRouter = express.Router();
 const jwt = require("jsonwebtoken");
-// const auth = require("../middlewares/auth");
+const auth = require("../middlewares/auth");
 
 // sign up
 authRouter.post("/api/signup", async (req, res) => {
@@ -59,26 +59,26 @@ authRouter.post("/api/signin", async (req, res) => {
     }
   });
   
-//   authRouter.post("/tokenIsValid", async (req, res) => {
-//     try {
-//       const token = req.header("x-auth-token");
-//       if (!token) return res.json(false);
-//       const verified = jwt.verify(token, "passwordKey");
-//       if (!verified) return res.json(false);
+  authRouter.post("/tokenIsValid", async (req, res) => {
+    try {
+      const token = req.header("x-auth-token");
+      if (!token) return res.json(false);
+      const verified = jwt.verify(token, "passwordKey");
+      if (!verified) return res.json(false);
   
-//       const user = await User.findById(verified.id);
-//       if (!user) return res.json(false);
-//       res.json(true);
-//     } catch (e) {
-//       res.status(500).json({ error: e.message });
-//     }
-//   });
+      const user = await User.findById(verified.id);
+      if (!user) return res.json(false);
+      res.json(true);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
   
-//   // get user data
-//   authRouter.get("/", auth, async (req, res) => {
-//     const user = await User.findById(req.user);
-//     res.json({ ...user._doc, token: req.token });
-//   });
+  // get user data
+  authRouter.get("/", auth, async (req, res) => {
+    const user = await User.findById(req.user);
+    res.json({ ...user._doc, token: req.token });
+  });
 
 module.exports = authRouter;
 
